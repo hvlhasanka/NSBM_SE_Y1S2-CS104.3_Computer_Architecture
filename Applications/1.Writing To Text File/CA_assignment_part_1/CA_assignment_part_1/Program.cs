ef23c5fbed3c12b164ce7a753d835d17ea52a12d
Lucas Lahiru.H.H <hvlhasanka@students.nsbm.lk>
2020-04-01 20:22:41 +0530

    Version 1
    Added latest source codes of the applications for writing and reading data from a text file.

---
+++ Applications/1.Writing To Text File/CA_assignment_part_1/CA_assignment_part_1/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Source Code Written By: Hewa Vidanage Lahiru Hasanka */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;//required to use built-in functions to retrieve hostname
+using System.IO;//required to use built-in fucntions to save information in text file
+
+namespace CA_assignment_part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating a new object in streamwriter and assigning the pathway of the text file
+            StreamWriter print_to_txt = new StreamWriter(@"C:\Users\Lucas.L.H.H\Desktop\Computer Architechture Assignment\CA_assignment_part_1\H.V.L.Hasanka(10026404).txt");
+
+            //retrieving hostname and assigning into a string variable
+            string hostname = Dns.GetHostName();
+            //using print_to_txt object name will save the required information into the text file
+            print_to_txt.WriteLine("Hostname: " + hostname);
+            //to display information to the console applicatione
+            Console.WriteLine("Hostname: " + hostname);
+
+            for (int i = 1; i <= 100; i++)
+            {
+                //retrieving current date and time of the system and assigning it to the current_time variable
+                DateTime current_time = DateTime.Now;
+                //by including the variable name and a sub section, the required component can be represented like hour, minute, second, and millisecond
+                print_to_txt.WriteLine("Iteration: " + i + "\t\tSystem time during iteration execution:  " + current_time.Hour + "HH : " + current_time.Minute + "MM : " + current_time.Second + "Sec . " + current_time.Millisecond + "Ms");
+                Console.WriteLine("Iteration: " + i + "\t\tSystem time during iteration execution:  " + current_time.Hour + "HH : " + current_time.Minute + "MM : " + current_time.Second + "Sec . " + current_time.Millisecond + "Ms");
+            }
+            //closing the link to the file
+            print_to_txt.Close();
+
+            //required to keep the concole application open after code execution, will be open until a key press is provided by the user input
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Applications/2.Reading From Text File/CA_assignment_part_2/CA_assignment_part_2/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Source Code Written By: Hewa Vidanage Lahiru Hasanka */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;//required to use buil-in functions to read information of the text file. 
+
+namespace CA_assignment_part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating a new object in streamreader and assigning the pathway of the text file
+            StreamReader read_from_txt = new StreamReader(@"C:\Users\Lucas.L.H.H\Desktop\Computer Architechture Assignment\CA_assignment_part_1\H.V.L.Hasanka(10026404).txt");
+            //will read the first line in text file
+            string retrieving_data = read_from_txt.ReadLine();
+            //this will continue to read the text file until the end is reached
+            while (retrieving_data != null)
+            {
+                //the read information will be displayed on to the console application
+                Console.WriteLine(retrieving_data);
+                //will read the next existing line
+                retrieving_data = read_from_txt.ReadLine();
+
+                //retrieving current date and time of the system and assigning it to the current_time variable
+                DateTime current_time = DateTime.Now;
+                //displaying only the appropriate sub sections of the time on the console application
+                Console.WriteLine("System time during data retrieval:  " + current_time.Hour + "HH : " + current_time.Minute + "MM : " + current_time.Second + "Sec . " + current_time.Millisecond + "Ms\n");
+            }
+            //closing the link to the file
+            read_from_txt.Close();
+            //required to keep the concole application open after code execution, will be open until a key press is provided by the user input
+            Console.ReadKey();
+        }
+    }
+}
+
